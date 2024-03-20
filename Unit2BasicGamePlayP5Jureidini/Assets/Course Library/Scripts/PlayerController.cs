339d5f5a1237151ab90d0d15ad230ffd3aef26ba
MrLongarms <704718@student.dusd.net>
2024-03-20 13:53:35 -0700

    2.2 done

--- Unit2BasicGamePlayP5Jureidini/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayP5Jureidini/Assets/Course Library/Scripts/PlayerController.cs
@@ -29,5 +29,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right *  horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a Projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
